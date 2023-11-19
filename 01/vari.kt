1ee753cf4872759824b4a847a26b83122e7978db
Ali Ashtab <aliashtab77@gmail.com>
2023-11-19 10:32:17 -0800

    جلسه نخست

---
+++ 01/vari.kt
@@ -0,0 +1,16 @@
+fun main(){
+//    1
+    var name:String
+    name = "ali"
+    println(name)
+    name = "zahra"
+    println(name)
+//    2
+var ageOfStudent = 20
+//    3
+    var salary = 30000
+    salary = 40000 //execute
+
+    val language = "Java"
+    language = "Kotlin" //Error
+}
